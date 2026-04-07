8c68e944c5da558495748b6b3c60c7b9486d08ea
Stanley Chiu <stanleychiu1234@gmail.com>
2026-04-07 02:25:58 -0700

    Blog Fix

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'The Bi-Weekly Bugle';
-export const SITE_DESCRIPTION = 'Home to the best blog on the internet.';
+export const SITE_DESCRIPTION = 'The best blog on the internet.';
