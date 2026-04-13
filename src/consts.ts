19e8b4ef44b94381a35ee6dfc62080eca7772946
Stanley Chiu <stanleychiu1234@gmail.com>
2026-04-13 03:49:11 -0400

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'The Bi-Weekly Bugle';
+export const SITE_DESCRIPTION = 'The best blog on the internet.';
